f1739aa694c640f1523d74649624fbe3a1470b23
ZackFiner <zackary.finer@yahoo.com>
2019-09-25 19:52:40 -0700

    Update Ray.h

--- src/Ray.h
+++ src/Ray.h
@@ -8,6 +8,7 @@ public:
 	glm::vec3 hitPos, hitNorm, hitDir;
 	float hitDist;
 	SceneObject* hitObject;
+
 	RayHit();
 	RayHit(const RayHit&);
 	RayHit& operator=(const RayHit&);
@@ -16,14 +17,22 @@ public:
 class Ray
 {
 	glm::vec3 dir, orig;
-	Ray(const glm::vec3& dir, const glm::vec3 orig);
-	RayHit getHit(std::vector<SceneObject*>);
+public:
 	Ray();
 	Ray(const Ray&);
+	Ray(const glm::vec3& dir, const glm::vec3 orig);
 	Ray& operator=(const Ray&);
+	RayHit getHit(std::vector<SceneObject*>);
 };
 
 class RayCam
 {
-private:
+	glm::vec3 origin;
+	glm::vec3  front, left, right;
+	float focalDist;
+public:
+	RayCam(const glm::vec3& f, const glm::vec3& r, const glm::vec3& u, const glm::vec3& o);
+	Ray getRay(const glm::vec2& coord, const glm::vec2& dim);
+	void setPos(const glm::vec3& newPos);
+	void rotate(const glm::quat& rot);
 };
