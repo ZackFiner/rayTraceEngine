64acccdf3ec08f946cdd1e931efa48a383588344
ZackFiner <zackary.finer@yahoo.com>
2019-09-25 19:47:43 -0700

    more work

--- src/Ray.h
+++ src/Ray.h
@@ -2,11 +2,15 @@
 #include "ofMain.h"
 
 class SceneObject;
-struct RayHit
+class RayHit
 {
+public:
 	glm::vec3 hitPos, hitNorm, hitDir;
 	float hitDist;
 	SceneObject* hitObject;
+	RayHit();
+	RayHit(const RayHit&);
+	RayHit& operator=(const RayHit&);
 };
 
 class Ray
@@ -14,9 +18,12 @@ class Ray
 	glm::vec3 dir, orig;
 	Ray(const glm::vec3& dir, const glm::vec3 orig);
 	RayHit getHit(std::vector<SceneObject*>);
+	Ray();
+	Ray(const Ray&);
+	Ray& operator=(const Ray&);
 };
 
 class RayCam
 {
-
+private:
 };
--- src/SceneObjects.h
+++ src/SceneObjects.h
@@ -1,1 +1,15 @@
 #pragma once
+#include "ofMain.h"
+class Ray;
+class RayHit;
+class SceneObject
+{
+public: 
+	virtual glm::vec3 getPos() = 0;
+	virtual glm::quat getRot() = 0;
+	virtual void draw() const = 0;
+	virtual RayHit castRay(const Ray& ray) const = 0;
+	virtual void setPos(const glm::vec3& newPos) = 0;
+	virtual void setPos(const glm::vec3& newRot) = 0;
+};
+
